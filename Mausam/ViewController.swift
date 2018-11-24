73c9ede4f8306ed2edac341724c4392e46f2c4af
Sanya Hassan <sania@devilss-macbook.local>
2018-11-24 23:43:56 +1100

    Initial Commit

---
+++ Mausam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mausam
+//
+//  Created by Sania on 24/11/18.
+//  Copyright © 2018 Sania. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
