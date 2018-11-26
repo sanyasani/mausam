963954c8bc5c0a61138096ec1e1b6bf86355c430
Sanya <>
2018-11-26 21:14:46 +1100

    Added UI for main screen

---
+++ Mausam/WeatherinfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  wetherinfoViewController.swift
+//  Mausam
+//
+//  Created by Sania on 26/11/18.
+//  Copyright © 2018 Sania. All rights reserved.
+//
+
+import UIKit
+
+class WeatherinfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func back(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
